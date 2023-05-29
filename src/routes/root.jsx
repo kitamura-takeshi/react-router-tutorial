d3a996f3c48a3b52c9e77408b83313db3a913d1f
kitamura.takeshi <kitamura.takeshi@plus-zero.co.jp>
2023-05-29 16:04:06 +0900

    active link styling

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { Link, Outlet, useLoaderData, Form, redirect, } from "react-router-dom";
+import { Outlet, useLoaderData, Form, redirect, NavLink, } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
 export async function action() {
@@ -43,7 +43,16 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink 
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) => 
+                      isActive
+                        ? "active"
+                        : isPending
+                        ? "pending"
+                        : ""
+                    }
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -52,7 +61,7 @@ export default function Root() {
                       <i>No Name</i>
                     )}{" "}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
