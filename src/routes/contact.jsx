ac232a0a76638582d583dfb00f724c319ca9bd7a
kitamura.takeshi <kitamura.takeshi@plus-zero.co.jp>
2023-05-29 17:07:35 +0900

    not found data

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -3,6 +3,12 @@ import { getContact, updateContact } from "../contacts";
 
 export async function loader({ params }) {
   const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("", {
+      status: 404,
+      statusText: "Not Found",
+    });
+  }
   return { contact };
 }
 
@@ -82,7 +88,7 @@ function Favorite({ contact }) {
   if (fetcher.formData) {
     favorite = fetcher.formData.get("favorite") === "true";
   }
-  
+
   return (
     <fetcher.Form method="post">
       <button
